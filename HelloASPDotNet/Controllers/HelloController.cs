3a313576eb8e666c9617a17f4526725cccfc01cf
josh-weiss76 <59374753+josh-weiss76@users.noreply.github.com>
2020-06-23 11:57:43 -0400

    practice with views

--- HelloASPDotNet/Controllers/HelloController.cs
+++ HelloASPDotNet/Controllers/HelloController.cs
@@ -6,62 +6,52 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace HelloASPDotNet.Controllers
 {
-    
+    //[Route("/helloworld")]
+
     public class HelloController : Controller
     {
         //GET: /<controller>/
-        [Route("/helloworld")]
         [HttpGet]
         public IActionResult Index()
         {
-            string html = "<form method='post' action='/helloworld/'>" + 
-                "<input type='text' name='name' />" + 
-                "<select name='language'>" +
-                    "<option value='english'>English</option>" +
-                    "<option value='french'>French</option>" +
-                    "<option value='spanish'>Spanish</option>" +
-                    "<option value='german'>German</option>" +
-                    "<option value='italian'>Italian</option>" +
-                "</select>" +
-                "<input type='submit' value='Greet Me!' />" + 
-                "</form>";
-            return Content(html, "text/html");
+            return View(); ;
         }
 
         // GET: /hello/welcome
-        [Route("/hello/{name?}")]
         [HttpPost]
+        [Route("/hello/{name?}")]
         public IActionResult Welcome(string name = "World")
         {
-            return Content("<h1>Welcome to my app, " + name + "!</h1>", "text/html");
+            ViewBag.person = name;
+            return View();
         }
 
-        [Route("/helloworld")]
-        [HttpPost]
-        public IActionResult CreateMessage(string name, string language)
-        {
-            if (language == "english")
-            {
-                return Content("<h1>Hello " + name + "!</h1>", "text/html");
-            }
-            else if (language == "french")
-            {
-                return Content("<h1>Bonjour " + name + "!</h1>", "text/html");
-            }
-            else if (language == "spanish")
-            {
-                return Content("<h1>Hola " + name + "!</h1>", "text/html");
-            }
-            else if (language == "german")
-            {
-                return Content("<h1>Hallo " + name + "!</h1>", "text/html");
-            }
-            else if (language == "italian")
-            {
-                return Content("<h1>Ciao " + name + "!</h1>", "text/html");
-            }
-            return Content("<h1>Welcome to my app, " + name + "!</h1>", "text/html");
-        }
+        
+        //[HttpPost]
+        //public IActionResult CreateMessage(string name, string language)
+        //{
+        //    if (language == "english")
+        //    {
+        //        return Content("<h1>Hello " + name + "!</h1>", "text/html");
+        //    }
+        //    else if (language == "french")
+        //    {
+        //        return Content("<h1>Bonjour " + name + "!</h1>", "text/html");
+        //    }
+        //    else if (language == "spanish")
+        //    {
+        //        return Content("<h1>Hola " + name + "!</h1>", "text/html");
+        //    }
+        //    else if (language == "german")
+        //    {
+        //        return Content("<h1>Hallo " + name + "!</h1>", "text/html");
+        //    }
+        //    else if (language == "italian")
+        //    {
+        //        return Content("<h1>Ciao " + name + "!</h1>", "text/html");
+        //    }
+        //    return Content("<h1>Welcome to my app, " + name + "!</h1>", "text/html");
+        //}
 
     }
 }
